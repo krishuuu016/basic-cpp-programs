0755c08a1711e93faac94428104fe275223c2ab0
krishuuu016 <rajputkrishuuu@gmail.com>
2024-10-26 15:28:15 +0530

    added new program - sum of number

---
+++ sumOfNoFrom1toN.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sumOfNumbers() {
+    cout << "enter your number :" << endl;
+    int num;
+    cin >> num;
+
+    int sum = 0;
+
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+
+    return sum;
+    
+}
+
+int main() {
+    int sum = sumOfNumbers();
+    cout << sum;
+
+}
